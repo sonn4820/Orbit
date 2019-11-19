48157d1930b4f6d172729c5d773d8274313b12a3
Son Nguyen <sonnguyen4820@gmail.com>
2019-11-18 19:46:14 -0500

    Title Scene

---
+++ Assets/Scripts/UI/Title.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void Start()
+    {
+        panel.gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Option1()
+    {
+        panel.gameObject.SetActive(true);
+    }
+    public void Option2()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
