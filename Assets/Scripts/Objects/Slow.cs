5b136ea2f51d02093e1318babf5ec74220610c4b
Son Nguyen <sonnguyen4820@gmail.com>
2019-11-29 15:57:47 -0500

    update1

---
+++ Assets/Scripts/Objects/Slow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slow : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().angularVelocity = 100;
+        
+    }
+}
