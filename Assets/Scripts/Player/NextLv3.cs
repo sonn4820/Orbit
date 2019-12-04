c8cf85b8fc04e43090e35a778eb3f78682c3021f
Son Nguyen <sonnguyen4820@gmail.com>
2019-12-03 19:59:06 -0500

    update

---
+++ Assets/Scripts/Player/NextLv3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLv3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "crystal")
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
