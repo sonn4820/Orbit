1759b0c3baf738821396a29077ed51462693d83f
Son Nguyen <sonnguyen4820@gmail.com>
2019-12-05 10:42:48 -0500

    Win Scene

---
+++ Assets/Scripts/Objects/FlashyWin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlashyWin : MonoBehaviour
+{
+    public GameObject flashing_Label;
+
+    public float interval;
+
+    void Start()
+    {
+        InvokeRepeating("FlashLabel", 0.6f, interval);
+    }
+
+    void FlashLabel()
+    {
+        if (flashing_Label.activeSelf)
+            flashing_Label.SetActive(false);
+        else
+            flashing_Label.SetActive(true);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
